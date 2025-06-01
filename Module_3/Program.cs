7cc1f6be85a445329c1411bfc3b086c772f3b65c
JC-1230 <97764196+JC-1230@users.noreply.github.com>
2025-06-01 18:23:44 -0400

    Add files via upload

---
+++ Module_3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {       
+        // Create an instance of the Bunny class
+        Bunny myBunny = new Bunny()
+        {
+            Name = "Bugs",
+            FavoriteFood = BunnyFood.Carrots,
+            Age = 3
+        };
+
+        // Display bunny information
+        Console.WriteLine($"{myBunny.Name} is {myBunny.Age} years old.");
+        Console.WriteLine($"{myBunny.Name} loves to eat {myBunny.FavoriteFood}.");
+        // Feed the bunny
+        myBunny.Eat();
+    }
+}
+
+// Class definition for Bunny
+public class Bunny
+{
+    public string Name = "";
+    public BunnyFood FavoriteFood;
+    public int Age;
+
+    public void Eat()
+    {
+        Console.WriteLine($"{Name} is eating {FavoriteFood}.");
+        Console.WriteLine("Bunny is happy!");
+    }
+}
+
+// Enum for Bunny's favorite food
+public enum BunnyFood
+{
+    Carrots,
+    Lettuce,
+    Cabbage,
+    Kale
+}
